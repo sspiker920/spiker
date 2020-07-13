0d39f3c036fa26e60b71e8ce37332b34a23480b2
spiker <spiker920@gmail.com>
2020-07-13 14:34:46 +0300

    first

---
+++ spiker1/src/Spik.java
@@ -0,0 +1,49 @@
+public class Spik {
+
+    public static void main(String... args) {
+        int a = 2;
+        int b = 12;
+        int c = 45;
+        int d = 54;
+        int i = 1;
+        int f = 43;
+        int g = 23;
+        int j = 2;
+        int result = a * b + c + (d - i) / f - g + j;
+        System.out.println("res1: " + result);
+        a = 12;
+        b = 12;
+        c = 45;
+        d = 10;
+        i = 11;
+        result = a * b - c / d + i;
+        System.out.println("res2: " + result);
+
+        a = 125;
+        b = 32;
+        c = 4;
+        d = 2;
+        i = 9;
+        f = 23;
+        g = 7;
+        j = 11;
+        int p = 8;
+        int o = 9;
+        result = a + b * c * d * i + f - g + j + p / o;
+        System.out.println("res3: " + result);
+
+        a = 9;
+        b = 11;
+        c = 34;
+        d = 2;
+        i = 1;
+        result = a + b - c / d + i;
+        System.out.println("res4: " + result);
+
+
+
+    }
+
+
+}
+
